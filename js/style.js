8333fbc80b97c3ba03c5698cfd44fd94ff7a9c3d
alaminjewel <72698595+alaminjewel@users.noreply.github.com>
2021-10-01 12:28:29 +0600

    Update style.js

--- js/style.js
+++ js/style.js
@@ -12,21 +12,3 @@ btn.on('click', function(e) {
   e.preventDefault();
   $('html, body').animate({scrollTop:0}, '300');
 });
-
-$('.owl-carousel').owlCarousel({
-    rtl:true,
-    loop:true,
-    margin:10,
-    nav:false,
-    autoplay:true,
-    autoplayTimeout:1500,
-    autoplayHoverPause:false,
-    responsive:{
-      600:{
-          items:3
-      },
-      1000:{
-          items:5
-      }
-    }
-  })
